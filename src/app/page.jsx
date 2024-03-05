9c400e9fd9263c9509019b467d2879a9a13bb474
notBigO <2003varunp@gmail.com>
2024-03-05 22:12:06 +0530

    added parallax scroll

---
+++ src/app/page.jsx
@@ -0,0 +1,16 @@
+import ParallaxScroll from "@/components/ParallaxScroll";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="flex min-h-screen flex-col items-center justify-center p-24">
+        <h1 className="font-mirtha text-[190px] tracking-wide bg-gradient-purple text-transparent bg-clip-text select-none hover:tracking-wider transition-all duration-500 swayam-text">
+          YOUR STAGE AWAITS!
+        </h1>
+      </div>
+
+      <ParallaxScroll />
+    </main>
+  );
+}
