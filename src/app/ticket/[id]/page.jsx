edda21a2bd93c01665ca3f163884c3130374d622
notBigO <2003varunp@gmail.com>
2024-04-19 18:54:10 +0530

    added ticket

--- src/app/ticket/[id]/page.jsx
+++ src/app/ticket/[id]/page.jsx
@@ -1,13 +1,17 @@
 import MainLayout from '@/components/MainLayout';
-import React from 'react';
+import PrintTicketButton from '@/components/PrintTicketButton/PrintTicketButton';
 import Ticket from '@/components/Ticket';
 
 const TicketPage = ({ params }) => {
   const { id } = params;
-
+  const TicketComponent = <Ticket id={id} />;
   return (
     <MainLayout>
-      <Ticket id={id} />
+      {TicketComponent}
+
+      <div className="flex w-full justify-center mt- md:-mt-16 mb-10">
+        <PrintTicketButton ticketComponent={TicketComponent} />
+      </div>
     </MainLayout>
   );
 };
