b3f12b038a739a85dcf5c89aa8504d4d56dbc7a8
Sidharth 'R3FluX' Sreejil <94698713+sid-js@users.noreply.github.com>
2024-04-28 08:26:06 +0530

    Fix NextAtuh Route

--- src/app/verify/[id]/page.js
+++ src/app/verify/[id]/page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import prisma from '@/utils/client';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../api/auth/[...nextauth]/route';
+import { authOptions } from '../../api/auth/[...nextauth]/route';
 import { redirect } from 'next/navigation';
 import Link from 'next/link';
 
