0026603b8eee7b8313e5fbf0633f5b620312c25d
notBigO <2003varunp@gmail.com>
2024-05-01 01:04:24 +0530

    fixed footer hyperlinks and hero text appearing black on some screens

--- src/components/MainEventCard.jsx
+++ src/components/MainEventCard.jsx
@@ -3,13 +3,13 @@ import { MainPinContainer } from './ui/Main3DCard';
 
 const MainEventCard = ({ date, title, organizer, image, href }) => {
   return (
-    <MainPinContainer className='' title={organizer} href={href}>
-      <div className='flex relative basis-full flex-col p-1 tracking-tight text-slate-100/50 sm:basis-1/2 min-w-52 md:min-w-72'>
-        <img src={image} className='w-full rounded-md' />
-        <h3 className='font-chamisty  bg-gradient-purple text-transparent bg-clip-text text-center text-3xl mt-3'>
+    <MainPinContainer className="" title={organizer} href={href}>
+      <div className="flex relative basis-full flex-col p-1 tracking-tight text-slate-100/50 sm:basis-1/2 min-w-52 md:min-w-72">
+        <img src={image} className="w-full rounded-md" />
+        <h3 className="font-chamisty  bg-gradient-purple text-transparent bg-clip-text text-center text-3xl mt-3">
           {title}
         </h3>
-        <h3 className='font-satoshi text-center text-gray-300 text-lg'>
+        <h3 className="font-satoshi text-center text-gray-300 text-lg">
           Mega Event
         </h3>
       </div>
